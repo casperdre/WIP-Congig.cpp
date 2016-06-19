def82864476d5d5e6ebfe26ca101ac24e3b2ee1c
Flint111 <arma3ccdg5flint@gmail.com>
2016-06-18 23:33:40 -0500

    Create config.cpp

---
+++ config.cpp
@@ -0,0 +1,23 @@
+{							
+	class ou_air					
+	{						
+		units[] = {"ou_ch_46", "ou_ch_53d"};					
+		weapons[] = {};					
+		requiredVersion = 1;					
+		requiredAddons[] = {"CUP_Core","A3_Air_F", "A3_Weapons_F", "A3_Soft_F", "A3_data_F", "A3_Characters_F"};					
+		author = "TF-31 Team";										
+	};						
+{										
+	class BLU_F									
+	{									
+		displayName = "NATO";								
+		priority = 1;								
+		side = 1;								
+		icon = "\a3\Data_f\cfgFactionClasses_BLU_ca.paa";								
+		flag = "\a3\Data_f\Flags\flag_nato_co.paa";								
+	};
+{						
+		class Helicopter				
+		{					
+			displayName = "CH-46e";				
+		};		
